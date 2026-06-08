f0e779f899bb8f20b61f418d33816b8749de9706
RSS1013 <rss1013@alu.ubu.es>
2026-06-08 11:49:33 +0200

    Se aplica correctamente la refactorización Replace TypeCode by State or Strategy
    Se aplica correctamente la refactorización Replace TypeCode by State or Strategy

--- src/ubu/gii/dass/refactoring/Movie.java
+++ src/ubu/gii/dass/refactoring/Movie.java
@@ -17,19 +17,19 @@ public class Movie {
 	public static final int NEW_RELEASE = 1;
 
 	private String _title;
-	private int _priceCode;
+	private Price _price;
 
 	public Movie(String title, int priceCode) {
 		_title = title;
-		set_priceCode(priceCode);
+		set_price(priceCode);
 	}
 
 	public int getPriceCode() {
-		return get_priceCode();
+		return get_price();
 	}
 
 	public void setPriceCode(int arg) {
-		set_priceCode(arg);
+		set_price(arg);
 	}
 
 	public String getTitle() {
@@ -63,14 +63,29 @@ public class Movie {
 	/**
 	 * @return the _priceCode
 	 */
-	private int get_priceCode() {
-		return _priceCode;
+	private int get_price() {
+		return _price.getPriceCode();
 	}
 
 	/**
 	 * @param _priceCode the _priceCode to set
 	 */
-	private void set_priceCode(int _priceCode) {
-		this._priceCode = _priceCode;
+	private void set_price(int priceCode) {
+		switch (priceCode) {
+		case REGULAR:
+			_price = new RegularPrice();
+			break;
+
+		case NEW_RELEASE:
+			_price = new NewReleasePrice();
+			break;
+
+		case CHILDRENS:
+			_price = new ChildrensPrice();
+			break;
+
+		default:
+			throw new IllegalArgumentException("Código de precio incorrecto");
+		}
 	}
 }
